ec0b5fa220b8b20887de8a6e3aae688df20dcf89
andretalyss <andretalyss1@gmail.com>
2023-05-05 18:28:34 -0300

    add cod

--- main.py
+++ main.py
@@ -1,111 +1,104 @@
+import bitarray
+N_BITS = 9
+tam_dicionario = 2**N_BITS
 dicionario = {}
-dicionario_inicial = {}
-message = ""
 
-def read_message():
-    arq = open("arquivo.txt", "r")
+def read_archive():
+    messagem = []
+    arq = open("arqui2.txt", "r")
     for linha in arq:
-        messagem = linha
+        for i in linha:
+            messagem.append(i)
+    
     arq.close()
 
     return messagem
 
-def dicionario_arranque(message):
-    codigo = 0
-    for caract in message:
-        if caract not in dicionario:
-            dicionario[f"{caract}"] = codigo
-            dicionario_inicial[f"{caract}"] = codigo
-            codigo +=1 
 
-    return codigo
+def lzw_compress(messagem):
+    dicionario = {chr(i): i for i in range(256)} # Inicializa dicionário ASCII
+    msg_comprimida = bitarray.bitarray()
 
-def codificacao_lzw(message, codigo):
-    saida = []
-    print(f"Messagem real: \n\t{message}")
-    aux_cod = codigo
-    s = message[0]
-    for caract in range(len(message)):                    
-        prox = caract+1
-        if prox >= len(message):
+    print(dicionario)
+    s = messagem[0] 
+    for caracter in range(len(messagem)):
+        if (caracter+1) >= len(messagem):
             for i in dicionario:
                 if s == i:
-                    saida.append(str(dicionario[i]))
+                    msg_comprimida.extend(bin(dicionario[i])[2:].zfill(N_BITS))
                     break
             
-            dicionario[f"{s}EOF"] = aux_cod
-            aux_cod+=1
+            if len(dicionario) == tam_dicionario:
+                print("Dicionário está cheio!")
+            else:
+                dicionario[f"{s}EOF"] = len(dicionario)
 
             break
-        else:    
-            c = message[caract+1]  
-
-        sequencia = [s, c]
+        else:
+            c = messagem[caracter+1]
+        
+        sequencia = [s,c]
         sequencia = ''.join(sequencia)
 
         if sequencia not in dicionario:
-            dicionario[f"{sequencia}"] = aux_cod
-            aux_cod +=1
+            if len(dicionario) == tam_dicionario:
+                print("Dicionário cheio!")
+            else:
+                dicionario[sequencia] = len(dicionario)
         else:
             s = sequencia
             continue
 
         for i in dicionario:
             if s == i:
-                saida.append(str(dicionario[i]))
+                msg_comprimida.extend(bin(dicionario[i])[2:].zfill(N_BITS))
                 break
-
+        
         s = c
 
-    print(f"\n{saida}")
-    print(f"\n{dicionario}")
-    return saida
-    
-            
-def decodifica_lzw(dicionario_inicial, codigo_comprimido, codigo):
-    saida_anterior = ""
-    saida_atual = ""
-    msg_decode = []     
-    flag = 0
-    for i in range(len(codigo_comprimido)+1):
-        s = saida_anterior
-        if i >= len(cod_comprimido):
-            dicionario_inicial[f"{s}EOF"] = codigo
-            codigo +=1
-            break
-        else:
-            c = codigo_comprimido[i]
-    
-        for k in dicionario_inicial:
-            if int(dicionario_inicial[k]) == int(c):
-                saida_atual = k
-                msg_decode.append(saida_atual)
-                flag = 1
-                break
-        
-        if not flag:
-            add = [s, s[0]]
-            add = ''.join(add)
-            dicionario_inicial[f"{add}"] = codigo
-            saida_atual = add
-            msg_decode.append(saida_atual)
-            codigo+=1
-            saida_anterior = saida_atual
-            continue
-        
-        flag = 0
-        saida_anterior = saida_atual
-        if s != "":
-            seq = [s, saida_atual[0]]
-            seq = ''.join(seq)
-            dicionario_inicial[f"{seq}"] = codigo
-            codigo +=1 
+    return msg_comprimida
 
-    msg_decode = ''.join(msg_decode)
-    print(f"\nMessagem descodificada: \n\t{msg_decode}")
-    print(f"\n{dicionario_inicial}")
-   
-message = read_message()
-cod = dicionario_arranque(message)
-cod_comprimido = codificacao_lzw(message, cod)
-decodifica_lzw(dicionario_inicial, cod_comprimido, cod)
+def lzw_decompress(messagem):
+    dicionario_2 = {i: chr(i) for i in range(256)}
+    saida_anterior = ""
+    msg_descomprimida = ""
+    code_size = N_BITS
+    index = 0
+
+    while index < len(messagem):
+        code = int(messagem[index:index+code_size].to01(), 2)
+        print(messagem[index:index+code_size].to01(), 2)
+        index+=code_size
+        # print(f"Index = {index}")
+        # print(f"Tamanho messagem: {len(messagem)}")
+
+        if code in dicionario_2:
+            # print(f"entoru no if: {code} - {len(dicionario_2)}")
+            saida_atual = dicionario_2[code]
+        elif code >= len(dicionario):
+            saida_atual = saida_anterior + saida_anterior[0]
+            # print(f"entrou no elif : {code} - {len(dicionario_2)}")
+        else:
+            # print(f"erro em {code}")
+            raise ValueError("Erro na compressão")
+        
+        msg_descomprimida += saida_atual
+        if saida_anterior:
+            if len(dicionario_2) == tam_dicionario:
+                print("Dicionário cheio")
+            else:
+                dicionario_2[len(dicionario_2)] = saida_anterior + saida_atual[0]    
+            # if len(dicionario_2) == 2**code_size:
+            #     code_size += 1
+        
+        saida_anterior = saida_atual
+
+    return msg_descomprimida
+    
+
+messagem = read_archive()
+compressed_data = lzw_compress(messagem)
+descompressed_data = lzw_decompress(compressed_data)
+print(messagem)
+print(compressed_data)
+print(descompressed_data)
