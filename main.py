fba04a007dd0cd630a6e03b9c58a1bde05173116
andretalyss <andretalyss1@gmail.com>
2023-05-08 20:03:45 -0300

    Att

--- main.py
+++ main.py
@@ -1,25 +1,34 @@
 import bitarray
-N_BITS = 9
+import sys
+
+arg1 = sys.argv[1]
+arg2 = sys.argv[2]
+
+N_BITS = 16
 tam_dicionario = 2**N_BITS
 dicionario = {}
 
 def read_archive():
-    messagem = []
-    arq = open("arqui2.txt", "r")
-    for linha in arq:
-        for i in linha:
-            messagem.append(i)
-    
-    arq.close()
+    if arg1 == 'txt':
+        messagem = ""
+        arq = open(arg2, "r")
+        for linha in arq:
+            for i in linha:
+                messagem += i
+        
+        arq.close()
 
-    return messagem
-
+        return messagem
+    elif arg1 == 'mp4':
+        with open(arg2, "rb") as file:
+            dados = file.read()
+            
+        return str(dados)
 
 def lzw_compress(messagem):
     dicionario = {chr(i): i for i in range(256)} # Inicializa dicionário ASCII
     msg_comprimida = bitarray.bitarray()
-
-    print(dicionario)
+    list_encoded = []
     s = messagem[0] 
     for caracter in range(len(messagem)):
         if (caracter+1) >= len(messagem):
@@ -28,11 +37,13 @@ def lzw_compress(messagem):
                     msg_comprimida.extend(bin(dicionario[i])[2:].zfill(N_BITS))
                     break
             
-            if len(dicionario) == tam_dicionario:
-                print("Dicionário está cheio!")
-            else:
-                dicionario[f"{s}EOF"] = len(dicionario)
+            # if len(dicionario) == tam_dicionario:
+            #     break
+            # else:
+            #dicionario[f"{s}EOF"] = len(dicionario)
 
+            ## FRACIONAR MENSAGEM COMPRIMIDA EM ARRAY
+            #list_encoded.append(msg_comprimida)
             break
         else:
             c = messagem[caracter+1]
@@ -42,7 +53,23 @@ def lzw_compress(messagem):
 
         if sequencia not in dicionario:
             if len(dicionario) == tam_dicionario:
-                print("Dicionário cheio!")
+            #     if len(list_encoded) == 2:
+            #         print(dicionario)
+                dicionario = {}
+                dicionario = {chr(i): i for i in range(256)}
+                #dicionario[sequencia] = len(dicionario)
+
+                for i in dicionario:
+                    if s == i:
+                        msg_comprimida.extend(bin(dicionario[i])[2:].zfill(N_BITS))
+                        break
+                s = c
+
+                ## FRACIONAR MENSAGEM COMPRIMIDA EM ARRAY
+                #list_encoded.append(msg_comprimida)
+                #msg_comprimida = bitarray.bitarray()
+                continue
+
             else:
                 dicionario[sequencia] = len(dicionario)
         else:
@@ -67,38 +94,48 @@ def lzw_decompress(messagem):
 
     while index < len(messagem):
         code = int(messagem[index:index+code_size].to01(), 2)
-        print(messagem[index:index+code_size].to01(), 2)
         index+=code_size
-        # print(f"Index = {index}")
-        # print(f"Tamanho messagem: {len(messagem)}")
+        print(f"Index = {index}")
+        print(f"Tamanho messagem: {len(messagem)}")
 
         if code in dicionario_2:
-            # print(f"entoru no if: {code} - {len(dicionario_2)}")
             saida_atual = dicionario_2[code]
-        elif code >= len(dicionario):
-            saida_atual = saida_anterior + saida_anterior[0]
-            # print(f"entrou no elif : {code} - {len(dicionario_2)}")
+        elif code >= len(dicionario_2):
+            if saida_anterior:
+                saida_atual = saida_anterior + saida_anterior[0]
+            else:
+                saida_atual = saida_anterior
         else:
-            # print(f"erro em {code}")
             raise ValueError("Erro na compressão")
         
         msg_descomprimida += saida_atual
         if saida_anterior:
             if len(dicionario_2) == tam_dicionario:
-                print("Dicionário cheio")
+                dicionario_2 = {}
+                dicionario_2 = {i: chr(i) for i in range(256)}
+                #dicionario_2[len(dicionario_2)] = saida_anterior + saida_atual[0] 
             else:
-                dicionario_2[len(dicionario_2)] = saida_anterior + saida_atual[0]    
-            # if len(dicionario_2) == 2**code_size:
-            #     code_size += 1
+                dicionario_2[len(dicionario_2)] = saida_anterior + saida_atual[0]   
         
         saida_anterior = saida_atual
 
     return msg_descomprimida
     
+def gerar_arquivo_comprimido(msg_comprimida):
+    with open('arquivo.lzw', 'w') as arquivo:
+        arquivo.write(msg_comprimida)
 
 messagem = read_archive()
 compressed_data = lzw_compress(messagem)
-descompressed_data = lzw_decompress(compressed_data)
-print(messagem)
-print(compressed_data)
-print(descompressed_data)
+print(len(compressed_data))
+
+if arg1 == 'txt':
+    decompress_data = lzw_decompress(compressed_data)
+    with open('compress', 'a') as arquivo:
+        for k in decompress_data:
+            arquivo.write(k)
+else:
+    decompress_data = []
+    for i in range(len(compressed_data)):
+        decompress_data.append(lzw_decompress(compressed_data[i]))
+
